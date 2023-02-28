eac4a65ddc62bef27a531d75b40f2c9b41fdb80c
maximiliano-mingarelli <maximiliano.mingarelli@hotmail.com>
2023-02-28 09:15:34 -0300

    experiencia estudios nav bar

--- proyecto-argentina-programa-mingarelli/src/app/app.component.spec.ts
+++ proyecto-argentina-programa-mingarelli/src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('proyecto-argentina-programa-mingarelli app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('proyecto-argentina-programa-mingarelli funciona!');
   });
 });
--- proyecto-argentina-programa-mingarelli/src/app/app.module.ts
+++ proyecto-argentina-programa-mingarelli/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
+import { RedesComponent } from './components/redes/redes.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoApComponent,
+    RedesComponent,
+    PortfolioComponent,
+    AboutMeComponent,
+    ExperienciaComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule
